6c531dc60f4917dc197525c037eebb9900efdea5
unel <enee.prog@gmail.com>
2025-11-15 18:48:22 +0100

    feat(UiProgress): add confetis

--- src/components/ui/UiProgress.vue
+++ src/components/ui/UiProgress.vue
@@ -5,7 +5,7 @@
         class="progress-bar-indicator"
         :style="{ width: (steps / max) * 100 + '%' }"
       />
-      <div class="motivationIcon">
+      <div class="motivationIcon" ref="motivationIconRef">
         {{ getRandom(motivationIcon.get(steps) ?? ["🔥"]) }}
       </div>
     </div>
@@ -18,6 +18,8 @@
 <script setup lang="ts">
 import { getRandom } from "@/tools/jsTools";
 import { useI18n } from "vue-i18n";
+import { ref, watch, nextTick } from "vue";
+import confetti from "canvas-confetti";
 
 const { steps } = defineProps<{
   max: number;
@@ -36,9 +38,31 @@ motivationIcon.set(6, ["💭", "🗣️", "🤝"]);
 motivationIcon.set(7, ["🎉", "🤯", "🙂‍↕️"]);
 motivationIcon.set(8, ["❤️", "😍", "🫵"]);
 
-const currentIcons = motivationIcon.get(steps) ?? ["🔥"];
-console.log(currentIcons);
-console.log(getRandom(currentIcons));
+const motivationIconRef = ref<HTMLElement | null>(null);
+
+// confeti on change
+watch(
+  () => steps,
+  async (newVal, oldVal) => {
+    if (oldVal !== undefined && newVal !== oldVal) {
+      await nextTick();
+      if (motivationIconRef.value) {
+        const el = motivationIconRef.value;
+        const rect = el.getBoundingClientRect();
+        //center of icon
+        const x = (rect.left + rect.right) / 2 / window.innerWidth;
+        const y = (rect.top + rect.bottom) / 2 / window.innerHeight;
+        confetti({
+          particleCount: 80,
+          spread: 720,
+          angle: 280,
+          startVelocity: 5,
+          origin: { x, y },
+        });
+      }
+    }
+  }
+);
 </script>
 
 <style lang="scss" scoped>
@@ -47,15 +71,16 @@ console.log(getRandom(currentIcons));
   flex-direction: column;
 
   .progress-bar-container {
-    height: 20px;
+    height: 25px;
     margin: 2px 2px;
     background: whitesmoke;
     position: relative;
+    align-items: center;
     border-radius: 25px;
     display: flex;
 
     .motivationIcon {
-      margin-left: -1em;
+      margin-left: -2em;
       z-index: 9;
     }
   }
@@ -63,6 +88,7 @@ console.log(getRandom(currentIcons));
   .progress-bar-indicator {
     height: 100%;
     border-radius: 25px;
+    transition-duration: 1000ms;
     /* this will do the magic */
     -webkit-mask: linear-gradient(#fff 0 0);
     mask: linear-gradient(#fff 0 0);
